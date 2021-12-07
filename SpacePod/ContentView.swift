59374e1b8b54f85c880758ed61382f3c54274102
Kern Jackson <kernjackson@gmail.com>
2021-12-06 19:22:39 -0600

    Initial Commit

---
+++ SpacePod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpacePod
+//
+//  Created by Kern Jackson on 12/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpacePod/SpacePodApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacePodApp.swift
+//  SpacePod
+//
+//  Created by Kern Jackson on 12/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SpacePodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
