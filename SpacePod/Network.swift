7170cc2ef1e089e8a93e63583e55614276772690
Kern Jackson <kernjackson@gmail.com>
2021-12-07 08:04:34 -0600

    Add getPod and date param

--- SpacePod/Network.swift
+++ SpacePod/Network.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class Network {
 
-    let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")!
+    let url = URL(string: "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2021-12-02")!
 
     func getString() async -> String {
         do {
@@ -12,6 +12,17 @@ class Network {
         }
         catch {
             return error.localizedDescription
+        }
+    }
+
+    func getPod() async -> Pod? {
+        do {
+            let request = URLRequest(url: url)
+            let (data, _) = try await URLSession.shared.data(for: request)
+            return try JSONDecoder().decode(Pod.self, from: data)
+        }
+        catch {
+            return nil
         }
     }
 }
